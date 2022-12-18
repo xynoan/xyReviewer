14e73f0c7e3a6baa67ffa88ab19d5aef504588de
Nathaniel Carlo <100833190+xynoan@users.noreply.github.com>
2022-12-18 16:37:48 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,53 @@
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] questionnaires = PhilippineMilitaryHistory.questionnaires;
+        String[] answerSheet = PhilippineMilitaryHistory.answerSheet;
+//        basic life support reviewer
+        String[] praises = {"Great job!", "Nice!", "Keep up the good work!", "Correct!"};
+        String[] motivations = {"You got this, try again!", "Don't give up!", "You're really close!"};
+        HashSet<String> alreadyAsked = new HashSet<>();
+        Random r = new Random();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to ROTC Finals Exam Reviewer!");
+        System.out.println("Press Enter to start.");
+        input.nextLine();
+        int upperbound = questionnaires.length;
+        int upperboundOfPraises = praises.length;
+        int upperboundOfMotivations = motivations.length;
+        int count = 0;
+        int point = 0;
+        System.out.println(questionnaires.length);
+        while (true) {
+            int random_questionnaire = r.nextInt(upperbound);
+            int random_praise = r.nextInt(upperboundOfPraises);
+            int random_motivation = r.nextInt(upperboundOfMotivations);
+            String question = questionnaires[random_questionnaire];
+            if (!alreadyAsked.contains(question)) {
+                count++;
+                System.out.println(count);
+                System.out.println(question);
+                alreadyAsked.add(question);
+            } else {
+                continue;
+            }
+            String answer = answerSheet[random_questionnaire];
+            String studentAnswer = input.nextLine();
+            if (studentAnswer.equalsIgnoreCase(answer)) {
+                System.out.println(praises[random_praise]);
+                point++;
+            } else {
+                System.out.println(motivations[random_motivation]);
+            }
+            if (count == questionnaires.length) {
+                break;
+            }
+        }
+        System.out.println("You got " + point + " points out of " + questionnaires.length + "!");
+    }
+}
