d0380f5a87e3c644c4c77d00f66b143f63c43c46
xynoan <ncmorva@gmail.com>
2024-05-12 12:29:48 +0800

    add reviewer for hci101

--- classes/Question.js
+++ classes/Question.js
@@ -1,5 +1,5 @@
 const Helper = new (require("./Helper.js"))();
-const PE4 = require("../subjects/PE4.js");
+const HCI101 = require("../subjects/HCI101.js");
 
 module.exports = class Question {
 
@@ -47,6 +47,6 @@ module.exports = class Question {
   }
 
   questionAndAnswer() {
-    return PE4;
+    return HCI101;
   }
 }
