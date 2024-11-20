2a90a13a225dabb24364dbafb086f7f7c12a4980
xynoan <ncmorva@gmail.com>
2024-11-20 17:22:30 +0800

    add questionnaire for IPT102

--- subjects/IPT102.js
+++ subjects/IPT102.js
@@ -1,20 +1,26 @@
 module.exports = [
+    // WEEK 5 - DEVELOPING VIEWS
     {
-        type: "multiple-choice",
-        question: "AR101",
-        options: ["A) 1", "B) 2", "C) 3", "D) 4"],
-        answer: "C"
+        "type": "multiple-choice",
+        "question": "One of the view engines supported in ASP.NET MVC. It allows you to write a mix of HTML and server-side code using C# or Visual Basic.",
+        "options": [
+            "A) Web Forms",
+            "B) Razor",
+            "C) Spark",
+            "D) NVelocity"
+        ],
+        "answer": "B"
     },
+    // WEEK 6 - DEVELOPING VIEWS - HTML Helpers
     {
-        type: "multiple-choice",
-        question: "AR101 second question",
-        options: ["A) 1", "B) 2", "C) 3", "D) 4"],
-        answer: "C"
-    },
-    {
-        type: "enumeration",
-        question: "List three primary colors.",
-        answer: ["Red", "Blue", "Yellow"],
-        orderMatters: false
+        "type": "multiple-choice",
+        "question": "Used to render the most common type of HTML controls.",
+        "options": [
+            "A) Web Forms Controls",
+            "B) Tag Helpers",
+            "C) Custom Scripts",
+            "D) HTML Helpers"
+        ],
+        "answer": "D"
     }
 ];
